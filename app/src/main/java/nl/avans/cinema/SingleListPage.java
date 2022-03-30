0a8148fb0ddd6b677658d38cabff57199b221a73
sbspanjers <sb.spanjers@gmail.com>
2022-03-30 09:27:40 +0200

    added new single list page

---
+++ app/src/main/java/nl/avans/cinema/SingleListPage.java
@@ -0,0 +1,13 @@
+package nl.avans.cinema;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingleListPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_list_page);
+    }
+}
