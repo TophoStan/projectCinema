9e44f7b0078d507ffc65c6e1c9a7df2fda43a700
sbspanjers <sb.spanjers@gmail.com>
2022-03-30 10:18:11 +0200

    made the single list page and changed some menu's

--- app/src/main/java/nl/avans/cinema/SingleListPage.java
+++ app/src/main/java/nl/avans/cinema/SingleListPage.java
@@ -2,6 +2,8 @@ package nl.avans.cinema;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class SingleListPage extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SingleListPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_list_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_menu,  menu);
+        return true;
     }
 }
