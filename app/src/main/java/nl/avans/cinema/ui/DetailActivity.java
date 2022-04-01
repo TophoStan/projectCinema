b9d6049cdf1daee7be2d1a8d09cdf813c7e114ba
sbspanjers <sb.spanjers@gmail.com>
2022-04-01 10:40:55 +0200

    working detailpage

--- app/src/main/java/nl/avans/cinema/ui/DetailActivity.java
+++ app/src/main/java/nl/avans/cinema/ui/DetailActivity.java
@@ -2,24 +2,15 @@ package nl.avans.cinema.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.view.Menu;
 import android.view.View;
-=======
-
-import android.view.View;
-
-import android.view.Menu;
->>>>>>> 4de2b5b455ed84120ffaa372ed8108d105f37739
 
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
 import nl.avans.cinema.R;
 import nl.avans.cinema.databinding.ActivityDetailBinding;
-import nl.avans.cinema.domain.Company;
 import nl.avans.cinema.domain.Genre;
 import nl.avans.cinema.domain.Movie;
 import nl.avans.cinema.ui.adapters.CompanyAdapter;
